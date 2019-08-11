f5879dc0c88d4ee92dbbdba4d1eb9d017764ee24
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-11 21:04:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,8 +14,10 @@ class School
   def add_student(name, grade)
     if @roster[grade]
       @roster[grade] << name
+    else 
     @roster[grade] = []
     @roster[grade] << name
+    end
   end
   
     
