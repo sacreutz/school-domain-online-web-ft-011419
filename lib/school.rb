ebee8271792abceb9a803d85c940446b99b7549b
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-11 20:48:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,18 +4,9 @@ class School
  
   def initialize(name)
     @name = name
+    @roster = {}
   end
   
- 
-
-  def roster=(roster = {})
-   
-    @roster = roster
-  end
-  
-   def roster
-    @roster
-  end
   
     
 end
